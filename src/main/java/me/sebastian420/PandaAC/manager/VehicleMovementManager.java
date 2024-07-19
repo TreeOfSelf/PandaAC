1338acc02f675de90e50e798a3b37399ea768b49
Sebastian <sebastian97@gmail.com>
2024-07-19 14:50:10 -0700

    Remove debug messages

--- src/main/java/me/sebastian420/PandaAC/manager/VehicleMovementManager.java
+++ src/main/java/me/sebastian420/PandaAC/manager/VehicleMovementManager.java
@@ -51,20 +51,16 @@ public class VehicleMovementManager {
             boolean blockUnder = BlockUtil.checkGroundVehicle(vehicle, packet.getY());
 
             if (blockUnder || blockStateUnder != Blocks.AIR.getDefaultState()) {
-
                 if (blockStateUnder.getBlock() == Blocks.WATER) {
                     speedPotential = SpeedLimits.BOAT_WATER;
                     yawPotential = SpeedLimits.BOAT_YAW_WATER;
-                    PandaLogger.getLogger().info("WATER");
                 } else if (blockStateUnder.isIn(BlockTags.ICE)) {
                     yawPotential = SpeedLimits.BOAT_YAW_ICE;
 
                     if (blockStateUnder.getBlock() == Blocks.BLUE_ICE) {
                         speedPotential = SpeedLimits.BOAT_BLUE_ICE;
-                        PandaLogger.getLogger().info("BLUE ICE");
                     } else {
                         speedPotential = SpeedLimits.BOAT_ICE;
-                        PandaLogger.getLogger().info("ICE");
                     }
                 } else {
                     speedPotential = SpeedLimits.BOAT_LAND;
@@ -74,7 +70,6 @@ public class VehicleMovementManager {
             } else {
                 speedPotential = SpeedLimits.BOAT_AIR;
                 yawPotential = SpeedLimits.BOAT_YAW_LAND;
-                PandaLogger.getLogger().info("AIR");
             }
         }
 
