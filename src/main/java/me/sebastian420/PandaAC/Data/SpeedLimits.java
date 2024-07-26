139c0ac1144db029df21a66f0875f1eeb22e6c4e
Sebastian <sebastian97@gmail.com>
2024-07-26 15:45:55 -0700

    Messing with elytra horizontal speed

--- src/main/java/me/sebastian420/PandaAC/Data/SpeedLimits.java
+++ src/main/java/me/sebastian420/PandaAC/Data/SpeedLimits.java
@@ -3,6 +3,8 @@ package me.sebastian420.PandaAC.data;
 public class SpeedLimits {
 
     /* trident rip tide
+    prob will need to keep speed when switching off elytra
+    status effects
      */
 
     public static final double FUDGE = 1.0;
@@ -46,7 +48,7 @@ public class SpeedLimits {
     public static final double STRIDER_SPEED = 5;
     public static final double STRIDER_YAW = 2000;
 
-    public static final double ELYTRA = 30;
+    public static final double ELYTRA = 40;
     public static final double ELYTRA_VERTICAL = 30;
 
 }
