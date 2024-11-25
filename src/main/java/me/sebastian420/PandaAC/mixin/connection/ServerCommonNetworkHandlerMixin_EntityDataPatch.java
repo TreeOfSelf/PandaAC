ce6f8c9ae8dc250de83399097558473361eef737
Sebastian <sebastian97@gmail.com>
2024-11-24 22:24:23 -0800

    Update ServerCommonNetworkHandlerMixin_EntityDataPatch.java

--- src/main/java/me/sebastian420/PandaAC/mixin/connection/ServerCommonNetworkHandlerMixin_EntityDataPatch.java
+++ src/main/java/me/sebastian420/PandaAC/mixin/connection/ServerCommonNetworkHandlerMixin_EntityDataPatch.java
@@ -21,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static me.sebastian420.PandaAC.PandaAC.pandaConfig;
@@ -36,16 +37,17 @@ public abstract class ServerCommonNetworkHandlerMixin_EntityDataPatch {
     @Shadow protected abstract GameProfile getProfile();
 
     @Shadow @Final protected MinecraftServer server;
+
+    @Shadow public abstract void sendPacket(Packet<?> packet);
+
     @Unique
     private static final TrackedData<Float> LIVING_ENTITY_HEALTH = LivingEntityAccessor.getHealth();
     @Unique
     private static final TrackedData<Float> PLAYER_ENTITY_ABSORPTION = PlayerEntityAccessor.getAbsorption();
 
 
-    @Inject(method = "send", at = @At("HEAD"))
+    @Inject(method = "send", at = @At("HEAD"), cancellable = true)
     private void onSend(Packet<?> packet, PacketCallbacks callbacks, CallbackInfo ci) {
-
-
         GameProfile profile = this.getProfile();
         ServerPlayerEntity player = this.server.getPlayerManager().getPlayer(profile.getId());
 
@@ -59,8 +61,20 @@ public abstract class ServerCommonNetworkHandlerMixin_EntityDataPatch {
 
             if (pandaConfig.packet.removeHealthTags && entity instanceof LivingEntity && entity.isAlive() && !(entity instanceof Saddleable)) {
                 if (entity.getType() == EntityType.PLAYER) {
-                    trackedValues.removeIf(trackedValue -> trackedValue.id() == LIVING_ENTITY_HEALTH.id());
-                    //trackedValues.removeIf(trackedValue -> trackedValue.id() == PLAYER_ENTITY_ABSORPTION.id());
+                    List<DataTracker.SerializedEntry<?>> modifiedValues = new ArrayList<>(trackedValues);
+
+                    boolean didRemove = modifiedValues.removeIf(trackedValue -> trackedValue.id() == LIVING_ENTITY_HEALTH.id());
+
+                    if (didRemove) {
+                        EntityTrackerUpdateS2CPacket newPacket = new EntityTrackerUpdateS2CPacket(
+                                trackerPacket.id(),
+                                modifiedValues
+                        );
+
+                        ci.cancel();
+
+                        this.sendPacket(newPacket);
+                    }
                 }
             }
         }
